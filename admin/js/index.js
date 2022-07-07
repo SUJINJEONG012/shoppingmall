db3f323333da527d06d50e67e92e8a985b298652
JEONGSUJIN012 <peekaboo32@naver.com>
2022-07-08 00:29:51 +0900

    스크립트 추가

--- admin/js/index.js
+++ admin/js/index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+
+const tabs = document.querySelectorAll('[data-tab-target');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        console.log(target);
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        });
+
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        });
+        target.classList.add('active')
+        tab.classList.add('active')
+    })
+})
