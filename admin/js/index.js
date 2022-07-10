0ec2422efe9c04ab1157dc29323a4abb6e4c2b8d
JEONGSUJIN012 <peekaboo32@naver.com>
2022-07-10 20:44:22 +0900

    관리자페이지서브페이지 생성,경로지정

--- admin/js/index.js
+++ admin/js/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-const tabs = document.querySelectorAll('[data-tab-target');
+const tabs = document.querySelectorAll('.tabs > li');
 const tabContents = document.querySelectorAll('[data-tab-content]');
 //아코디언,
 const acc = document.querySelectorAll('.accordion');
@@ -10,7 +10,6 @@ const acc = document.querySelectorAll('.accordion');
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget)
-        console.log(target);
         tabContents.forEach(tabContent => {
             tabContent.classList.remove('active')
         });
@@ -24,6 +23,10 @@ tabs.forEach(tab => {
 })
 
 
+
+
+
+
 //아코디언 
 /*<div class="accordion">쇼핑몰정보</div>
 <div class="panel">
@@ -31,14 +34,14 @@ tabs.forEach(tab => {
         <li><a href="">내용1</a></li>
     </ul>
 </div>*/
-for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
+// for (let i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function () {
+//         this.classList.toggle("active");
+//         let panel = this.nextElementSibling;
+//         if (panel.style.maxHeight) {
+//             panel.style.maxHeight = null;
+//         } else {
+//             panel.style.maxHeight = panel.scrollHeight + "px";
+//         }
+//     });
+// }
