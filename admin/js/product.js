2aeddcc966ff720a2600604bc5d9a3cde227c924
SUJINJEONG012 <peekaboo32@naver.com>
2022-08-16 00:00:28 +0900

    상품 분류 추가 부분 구현

--- admin/js/product.js
+++ admin/js/product.js
@@ -1,39 +1,48 @@
 'use strict';
 
 const todoArray = []; //할 일들을 배열로 선언
+
+const main__titles = document.querySelector('.main__title');
+const add = document.querySelector('.add');
+
 let todoList = document.getElementById('todo__list').value;
 
 let inputButton = document.querySelector('.input__button');
 inputButton.addEventListener('click', addTodo);
 // addEventListener(이벤트유형, 이벤트인터페이스를 구현할 "객체" 또는 "함수")
 
+
+// search을 클릭하면  search에  active 클래스를 토글
+main__titles.addEventListener('click', () => {
+    add.classList.toggle('active');
+    input.focus();
+})
+
+
+
+
 function addTodo() {
     //input에 입력한 todo
     const todo = document.querySelector('.todoItem').value;
 
-    // if(todo === '' || todo === null){
-    //     alert('할일 리스트를 입력해주세요.');
-    //     document.querySelector('.todoItem').value = '';
-    //     document.querySelector('.todoItem').focus();
-    //     return;
 
-    // }else if(todo !== '' || todo !== null){
-    //     alert('할일 리스트가 추가되었습니다.');
-    //     todoArray.push(todo);
-    //     document.querySelector('.todoItem').value ='';
-    //     document.querySelector('.todoItem').focus();
-    //     console.log(todo);
-
-
-    // }
 
     //리스트 보여주는 변수 생성
     const li = document.createElement("li");
-    const deleteBtn = document.createElement('button');
+    const modifyBtn = document.createElement('div');
+    const deleteBtn = document.createElement('div');
+    modifyBtn.classList = 'modify_btn';
+    deleteBtn.className = 'delete_btn'; //클래스이름 생성
+
     // li.id ='test';
 
     //할일 목록 추가 값
     document.getElementById("todo__list").appendChild(li).innerText = todo;
+
+    //수정 하기 버튼
+    li.appendChild(modifyBtn);
+    modifyBtn.textContent = '수정하기';
+
 
     //삭제 하기 버튼
     li.appendChild(deleteBtn);
@@ -56,12 +65,5 @@ function addTodo() {
 
     }
     // addEventListener(이벤트유형, 이벤트인터페이스를 구현할 "객체" 또는 "함수")
-
-
-
-
-
-
-
 
 } //addTodo 함수 끝
