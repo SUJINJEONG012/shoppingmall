15576e43e1a1b703f77dddaaff6914036ac99a8f
JB <qwq140@gmail.com>
2022-08-17 02:17:25 +0900

    카테고리 수정 삭제

--- admin/js/product.js
+++ admin/js/product.js
@@ -106,73 +106,190 @@ function addTodoSlom() {
 
     const todoListUl = document.getElementById("todo__list");
 
-    let html = `<li id="todo_li_${index}">${todo}
+    let html = `<li id="todo_li_${index}">
+                    <div id="todo_text_${index}">${todo}</div>
                     <div class="modbtn">
                         <div class="add_btn" id="depth_add_btn_${index}" onclick="depthInputAdd(${index})">추가하기</div>
-                        <div class="modify_btn" id="modify_btn_${index}" onclick="depthModify(${index})">수정하기</div>
-                        <div class="delete_btn" id="delete_btn_${index}" onclick="depthDelete(${index})">제거하기</div>
+                        <div class="modify_btn" id="modify_btn_${index}" onclick="categoryModify(${index})">수정하기</div>
+                        <div class="delete_btn" id="delete_btn_${index}" onclick="categoryDelete(${index})">제거하기</div>
                     </div>
                 </li>`;
     todoListUl.insertAdjacentHTML("beforeend", html);
-
-
-
-
-
 }
 
 
 
+function categoryModify(index){
+    const todoLi = document.getElementById(`todo_li_${index}`);
+    let text = document.getElementById(`todo_text_${index}`).innerText;
+    console.log(text);
+    let html = `<input type="text" id="category_modify_input_${index}" value="${text}"/>
+                <div class="modbtn">
+                    <div class="btn_confirm" id="btn_confirm_${index}">확인</div>
+                    <div class="btn_cancel" id="btn_cancel_${index}">취소</div>
+                </div>`;
+
+    // 자식 노드 전부 삭제
+    while(todoLi.hasChildNodes()){
+        todoLi.removeChild( todoLi.firstChild );
+    }
+
+    todoLi.insertAdjacentHTML("beforeend", html);
+
+    let btnConfirm = document.getElementById(`btn_confirm_${index}`);
+    btnConfirm.addEventListener("click", ev => modifyConfirm(index));
+
+    let btnCancel = document.getElementById(`btn_cancel_${index}`);
+    btnCancel.addEventListener("click", ev => modifyCancel(index, text));
+}
+
+function modifyConfirm(index){
+    const todoLi = document.getElementById(`todo_li_${index}`);
+    let modifyInputValue = document.getElementById(`category_modify_input_${index}`).value;
+    while(todoLi.hasChildNodes()){
+        todoLi.removeChild( todoLi.firstChild );
+    }
+
+    let html = `<div id="todo_text_${index}">${modifyInputValue}</div>
+                    <div class="modbtn">
+                        <div class="add_btn" id="depth_add_btn_${index}" onclick="depthInputAdd(${index})">추가하기</div>
+                        <div class="modify_btn" id="modify_btn_${index}" onclick="categoryModify(${index})">수정하기</div>
+                        <div class="delete_btn" id="delete_btn_${index}" onclick="categoryDelete(${index})">제거하기</div>
+                    </div>`;
+
+    todoLi.insertAdjacentHTML("beforeend", html);
+}
+
+function modifyCancel(index, value){
+    const todoLi = document.getElementById(`todo_li_${index}`);
+    while(todoLi.hasChildNodes()){
+        todoLi.removeChild( todoLi.firstChild );
+    }
+
+    let html = `<div id="todo_text_${index}">${value}</div>
+                    <div class="modbtn">
+                        <div class="add_btn" id="depth_add_btn_${index}" onclick="depthInputAdd(${index})">추가하기</div>
+                        <div class="modify_btn" id="modify_btn_${index}" onclick="categoryModify(${index})">수정하기</div>
+                        <div class="delete_btn" id="delete_btn_${index}" onclick="categoryDelete(${index})">제거하기</div>
+                    </div>`;
+
+    todoLi.insertAdjacentHTML("beforeend", html);
+}
+
+function categoryDelete(index){
+    const todoLi = document.getElementById(`todo_li_${index}`);
+    todoLi.remove();
+}
 
 
-
-
-
+/// 하위 카테고리
 function depthInputAdd(num) {
     const refLi = document.getElementById(`todo_li_${num}`);
     let listSub = document.getElementById(`list_sub_${num}`);
     if (listSub == null) {
-        let html = `<div id="list_sub_${num}">
-                        <ul class="list_sub_ul">
-                        </ul>
-                     </div>`;
+        let html = `<li id="list_sub_${num}">
+                        <ul class="list_sub_ul"></ul>
+                     </li>`;
         refLi.insertAdjacentHTML("afterend", html);
         listSub = document.getElementById(`list_sub_${num}`);
     }
 
     let listSubUl = listSub.getElementsByClassName('list_sub_ul')[0];
 
-    let html = `<li id="list_sub_li">
-                        <div class="depth_input_container">
+    let html = `<li id="todo_sub_li">
                             <input type="text" class="depth_input"/>
                             <button type="button" class="input__button" onclick="depthAdd(this)">
                                 <i class="fas fa-plus-circle"></i>
                             </button>
-                        </div>
                 </li>`
 
     listSubUl.insertAdjacentHTML("beforeend", html);
 }
 
 
-
-
-
-
 function depthAdd(e) {
     // console.log(e);
     let parentDiv = e.parentNode;
+    let todoSubLi = parentDiv.parentNode;
     // console.log(parentDiv)
     let inputValue = parentDiv.getElementsByClassName('depth_input')[0].value;
     // console.log(inputValue);
 
-    let html = `<li id="todo_sub_li_${index}">${inputValue}
+    let html = `<li id="todo_sub_li">
+                    <div class="depth_text">${inputValue}</div>
                     <div class="modbtn">
-                        <div class="modify_btn" id="modify_btn_${index}">수정하기</div>
-                        <div class="delete_btn" id="delete_btn_${index}">제거하기</div>
+                        <div class="modify_btn" onclick="depthModify(this)">수정하기</div>
+                        <div class="delete_btn" onclick="depthDelete(this)">제거하기</div>
                     </div>
                 </li>`;
 
-    parentDiv.insertAdjacentHTML("afterend", html);
-    parentDiv.remove();
+    todoSubLi.insertAdjacentHTML("afterend", html);
+    todoSubLi.remove();
+}
+
+function depthDelete(e){
+    let todoSubLi = e.parentNode.parentNode;
+    let listSubUl = todoSubLi.parentNode;
+    todoSubLi.remove();
+    if(!listSubUl.hasChildNodes()){
+        let listSubLi = listSubUl.parentNode;
+        listSubLi.remove();
+    }
+}
+
+function depthModify(e){
+    let todoSubLi = e.parentNode.parentNode;
+    let textDiv = todoSubLi.getElementsByClassName('depth_text')[0];
+    let value = textDiv.innerText;
+    console.log(value);
+
+    while(todoSubLi.hasChildNodes()){
+        todoSubLi.removeChild( todoSubLi.firstChild );
+    }
+
+    let html = `<input type="text" class="depth_input" value="${value}"/>
+                <div class="modbtn">
+                    <div class="confirm_btn">확인</div>
+                    <div class="cancel_btn">취소</div>
+                </div>`
+    todoSubLi.insertAdjacentHTML("beforeend", html);
+
+    let btnConfirm = todoSubLi.getElementsByClassName('confirm_btn')[0];
+    btnConfirm.addEventListener("click", ev => depthModifyConfirm(todoSubLi));
+
+    let btnCancel = todoSubLi.getElementsByClassName('cancel_btn')[0];
+    btnCancel.addEventListener("click", ev => depthModifyCancel(todoSubLi, value));
+}
+
+function depthModifyConfirm(todoSubLi){
+    let modifyInputValue = todoSubLi.getElementsByClassName('depth_input')[0].value;
+    console.log(modifyInputValue);
+    while(todoSubLi.hasChildNodes()){
+        todoSubLi.removeChild( todoSubLi.firstChild );
+    }
+
+    let html = `<div class="depth_text">${modifyInputValue}</div>
+                    <div class="modbtn">
+                        <div class="modify_btn" onclick="depthModify(this)">수정하기</div>
+                        <div class="delete_btn" onclick="depthDelete(this)">제거하기</div>
+                    </div>`;
+
+    todoSubLi.insertAdjacentHTML("beforeend", html);
+}
+
+function depthModifyCancel(todoSubLi, value){
+    console.log('취소');
+    while(todoSubLi.hasChildNodes()){
+        todoSubLi.removeChild( todoSubLi.firstChild );
+    }
+
+    let html = `<div class="depth_text">${value}</div>
+                <div class="modbtn">
+                    <div class="modify_btn" onclick="depthModify(this)">수정하기</div>
+                    <div class="delete_btn" onclick="depthDelete(this)">제거하기</div>
+                </div>`;
+
+    console.log(html);
+
+    todoSubLi.insertAdjacentHTML("beforeend", html);
 }
